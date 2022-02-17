cc53ef92bdaedbc77228b61b0ca5931ace76b044
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-02-17 16:33:48 +1300

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Forrest Wilson-Jennings on 17/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
